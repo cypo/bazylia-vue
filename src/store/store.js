31a88c9bdc0af19a3e207f63fd7bff6308e9a3e6
cypo <cyprianpacyna@gmail.com>
2020-10-30 17:00:43 +0100

    fix for not-refreshed months

--- src/store/store.js
+++ src/store/store.js
@@ -61,7 +61,8 @@ export default new Vuex.Store({
     rozliczeniaFirma: {},
     faktura: null,
     companies: [],
-    wizyty: []
+    wizyty: [],
+    month: {}
   },
   getters: {
     getPatients: state => {
